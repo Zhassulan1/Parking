9a53b66553860a20fefdecdc4e597e42fcbf4a2b
Damir <kdamir2004@gmail.com>
2024-05-05 18:52:53 +0500

    4

--- parking_back/parking_back/settings.py
+++ parking_back/parking_back/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'api',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
